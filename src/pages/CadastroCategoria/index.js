b6d241c390b4afb17c0ff9c15b09f94709a9b450
danvb112 <danielbonasser@gmail.com>
2020-07-29 18:58:46 -0300

    Criacao de formulario

--- src/pages/CadastroCategoria/index.js
+++ src/pages/CadastroCategoria/index.js
@@ -1,29 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../components/PageDefault/index';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+    const [categorias, setCategorias] = useState(['Teste'])
+
+    const valoresIniciais = {
+        nome: 'Nome inicial',
+        descricao: ' descrição inicial',
+        cor: '#000'
+    }
+    const [values, setvalues] = useState(valoresIniciais);
     return (
         <div>
             <PageDefault>
-                <h1>Cadastro De Categoria</h1>
-                <form>
-                    <label>
-                        Nome da Categoria:
-                        <input
-                        type="text"
-                        />
-                    </label>
+                <h1>Cadastro De Categoria: {values.nome}</h1>
+                <form onSubmit={ function handleSubmit(infoDosEventos) {
+                    infoDosEventos.preventDefault();
+                    console.log('voce enviou um form')
+                    setCategorias([
+                        ...categorias,
+                        values
+                    ])
+                } }>
+
+                    <div>
+                        <label>
+                            Nome da Categoria: 
+                            <input
+                            type="text"
+                            value={values.nome}
+                            onChange={ function funcaoHandlerQueErroPediu(infoDosEventos){
+                                //setNomedaCategoria(infoDosEventos.target.value)
+                            } }
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Descrição: 
+                            <textarea
+                            type="text"
+                            value={values.descricao}
+                            onChange={ function funcaoHandlerQueErroPediu(infoDosEventos){
+                                //setNomedaCategoria(infoDosEventos.target.value)
+                            } }
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Cor: 
+                            <input
+                            type="color"
+                            value={values.cor}
+                            onChange={ function funcaoHandlerQueErroPediu(infoDosEventos){
+                               //setNomedaCategoria(infoDosEventos.target.value)
+                            } }
+                            />
+                        </label>
+                    </div>
+
 
                 <button>
                     Cadastrar
                 </button>
                 </form>
 
+                <ul>                
+                    {categorias.map((categoria, indice) => {
+                        return (
+                            <li key={`${categoria}${indice}`}>
+                                {categoria}
+                            </li>
+                    )
+                })}
+                </ul>
+
                 <Link to='/'>
                     Ir para Home
                 </Link>
             </PageDefault>
+
+            
                 
         </div>
     )
